3bfb2c2a4b28d851e602d65ccd58021a59f82ea5
Dario Clavijo <clavijodario@gmail.com>
2026-05-13 16:11:36 -0300

    Bump version: 0.1.4 → 0.1.5

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -10,7 +10,7 @@ def test_import():
     """Test that impactguard can be imported."""
     import impactguard
 
-    assert impactguard.__version__ == "0.1.4"
+    assert impactguard.__version__ == "0.1.5"
 
 
 def test_extract():
